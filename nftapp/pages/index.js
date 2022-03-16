2a7d8fca25e2fe7b5b7d17cdabb34e91abdc6272
Quentin Flachaire <quentin.flachaire@gmail.com>
2022-03-16 21:11:48 +0100

    ouo

---
+++ nftapp/pages/index.js
@@ -0,0 +1,15 @@
+export default function App() {
+
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="banniere">
+                    <Menu />
+                    <p> Welcome my friend! </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+}
